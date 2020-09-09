225628f6136db2fec20e80d1c06a59c290f23d26
Евгений <github.com@mpak.su>
2020-09-09 09:50:15 +0300

    Добавление alias pre

--- bmf.sh
+++ bmf.sh
@@ -6,14 +6,13 @@
 #sqlite3 ./2 -column -header "SELECT * FROM mp_bmf_test ORDER BY id DESC LIMIT 10; -- SELECT COUNT(*) as step, SUM(change) as SumChange, MAX(bmf) as bmf FROM mp_bmf_test WHERE perc<10;"
 ##sqlite3 ./2 -column -header "SELECT * FROM mp_bmf_index ORDER BY id DESC"
 
-cat clump/iris.json | ./bimorph - clump/_iris.sqlite -i 1
 #cat clump/tri.json | ./bimorph - ./3 -c -e 100
 #sqlite3 ./3 -column -header "SELECT * FROM mp_bmf_test ORDER BY id DESC LIMIT 10; -- SELECT COUNT(*) as step, SUM(change) as SumChange, MAX(bmf) as bmf FROM mp_bmf_test WHERE perc<10;"
 ##sqlite3 ./3 -column -header "SELECT * FROM mp_bmf_index ORDER BY id DESC"
 ##./bimorph ./3 -j '[{"dano":{"Один":"0", "Два":"0", "Три":"0"}}]'
 
-#cat clump/iris.json | ./bimorph - clump/iris.sqlite -c -e 100
-#sqlite3 clump/iris.sqlite -column -header "SELECT * FROM mp_bmf_test ORDER BY id DESC LIMIT 10; -- SELECT COUNT(*) as step, SUM(change) as SumChange, MAX(bmf) as bmf FROM mp_bmf_test WHERE perc<10;"
+cat clump/iris.json | ./bimorph - clump/iris.sqlite -c -e 100
+sqlite3 clump/iris.sqlite -column -header "SELECT * FROM mp_bmf_test ORDER BY id DESC LIMIT 10; -- SELECT COUNT(*) as step, SUM(change) as SumChange, MAX(bmf) as bmf FROM mp_bmf_test WHERE perc<10;"
 #	#sqlite3 clump/iris.sqlite -column -header "SELECT MAX(depth) FROM mp_bmf_index"
 #./bimorph clump/iris.sqlite -j '[{"dano":{"ДлиннаЧашелистика":"2.0","ШиринаЧашелистика":"4.0","ДлиннаЛепестка":"1.8","ШиринаЛепестка":"1.8"}}]'
 #./bimorph clump/iris.sqlite -j '[{"dano":{"ДлиннаЧашелистика":"5.0","ШиринаЧашелистика":"3.3","ДлиннаЛепестка":"1.4","ШиринаЛепестка":"0.2"}}]'
