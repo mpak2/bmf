9de958599b00c316a373a8d47540e2c05ddcdcf3
Евгений <github.com@mpak.su>
2020-09-23 08:57:02 +0300

    Правка утилиты csv2json

--- inc/f2.sh
+++ inc/f2.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-gcc csv2json.cpp -lstdc++ -std=c++17 -lsqlite3 -o ./csv2json && ./csv2json train_features.csv cp_time,cp_dose 0 sig_id,cp_type
+gcc csv2json.cpp -lstdc++ -std=c++17 -lsqlite3 -o ./csv2json #&& ./csv2json train_features.csv cp_time,cp_dose 0 sig_id,cp_type
