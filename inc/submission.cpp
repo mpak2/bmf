773b3fa4e90b9fffd4d2fbf5d09cbf9f19ba0b75
Евгений <github.com@mpak.su>
2020-10-08 06:13:08 +0300

    Утилита формирования файла ответов submission.csv

--- inc/submission.cpp
+++ inc/submission.cpp
@@ -25,10 +25,8 @@ int main(int argc, char **argv){
 			}; std::cerr << ")\n"; return false; }()){ mpre("ОШИБКА отображения массива", __LINE__);
 		}else{
 		} return false; }; false){ mpre("ОШИБКА установки функции", __LINE__);
-	}else if([&](){ // Формирование файла ответа
-		if(1 < argc){ //mpre("Не выводим результат", __LINE__);
-		}else if(std::map<int, std::map<std::string, std::string>> TAB; false){ mpre("ОШИБКА создания вложенного массива значений из БД", __LINE__);
-		}else if(sqlite3* db = 0; false){ mpre("ОШИБКА установки идентификатора ошибки базы данных", __LINE__);
+	}else if(std::map<int, std::map<std::string, std::string>> TAB = [&](std::map<int, std::map<std::string, std::string>> TAB = {}){ // Формирование файла ответа
+		if(sqlite3* db = 0; false){ mpre("ОШИБКА установки идентификатора ошибки базы данных", __LINE__);
 		}else if(std::string dbname = "submission.sqlite"; dbname.empty()){ mpre("ОШИБКА установки файла БД", __LINE__);
 		}else if(SQLITE_OK != sqlite3_open(dbname.c_str(), &db)){ mpre(" ОШИБКА открытия базы данных "+ dbname, __LINE__);
 		}else if(sqlite3_stmt* stmt; false){ mpre("Ссылка на ошибки", __LINE__);
@@ -44,57 +42,23 @@ int main(int argc, char **argv){
 					std::string value = (val == "NULL" ? "" : val);
 					row.insert(std::make_pair(field, value));
 				} //mpre(row, __LINE__);
-				TAB.insert(std::make_pair(TAB.size(), row));
+				if(row.end() == row.find("_id")){ mpre("ОШИБКА поле не содержит _id", __LINE__);
+				}else{ TAB.insert(std::make_pair(atoi(row.at("_id").c_str()), row));
+				}
 			}; return false; }()){ mpre("ОШИБКА получения данных запроса", __LINE__);
-		//}else if(std::cout << "Id,SalePrice" << std::endl; false){ mpre("ОШИБКА вывода заголовка файла", __LINE__);
-		}else if(std::map<std::string, std::string> TEST = [&](std::map<std::string, std::string> TEST = {}){ // Формирование списка который должен быть
-			for(int i = 1461; i <= 2919; i++){
-				TEST.insert(make_pair(std::to_string(i), std::to_string(i)));
-			} return TEST; }(); TEST.empty()){ mpre("ОШИБКА формирования списка нужных", __LINE__);
-		//}else if(true){ dump(TAB, __LINE__);
+		}else if(1 < argc){ //mpre("Не выводим результат", __LINE__);
+		}else if(std::cout << "id,target" << std::endl; false){ mpre("ОШИБКА вывода заголовка файла", __LINE__);
 		}else if(std::string text = [&](std::string text = "id,SalePrice\n"){ for(auto tab_itr:TAB){
 				if(tab_itr.second.end() == tab_itr.second.find("_id")){ mpre("ОШИБКА поле идентификатора не найдено в данных", __LINE__);
 				}else if(std::string id = tab_itr.second.at("_id"); id.empty()){ mpre("ОШИБКА выборки идентификатора", __LINE__);
-				}else if(tab_itr.second.end() == tab_itr.second.find("price")){ mpre("ОШИБКА поле цены не найдено в данных", __LINE__);
-				}else if(std::string price = tab_itr.second.at("price"); id.empty()){ mpre("ОШИБКА выборки идентификатора", __LINE__);
-				}else if(text += id+ "," +price +"\n"; text.empty()){ mpre("ОШИБКА добавления данных в содержимое csv", __LINE__);
-				}else if(TEST.erase(id); false){ mpre("ОШИБКА удаление уже добавленых", __LINE__);
-				//}else if(std::cout << id << "," << price << std::endl; false){ mpre("ОШИБКА формирования текста ответов csv", __LINE__);
+				}else if(tab_itr.second.end() == tab_itr.second.find("predict")){ mpre("ОШИБКА поле прогноза не найдено в данных", __LINE__);
+				}else if(std::string predict = tab_itr.second.at("predict"); predict.empty()){ mpre("ОШИБКА выборки прогноза", __LINE__);
+				}else if(text += id+ "," +predict +"\n"; text.empty()){ mpre("ОШИБКА добавления данных в содержимое csv", __LINE__);
 				}else{ //pre(tab_itr.second, __LINE__); mpre("Данные из таблицы", __LINE__);
 				}
 			} return text; }(); false){ mpre("ОШИБКА отображения значений таблицы", __LINE__);
-		}else if(!TEST.empty()){ /*dump(TEST, __LINE__);*/ std::cout << "./submission test.txt 0 0 "; for(auto test_itr:TEST){ std::cout << "," << test_itr.first; }; std::cout << std::endl; mpre("В табе не ватает полей size=" +std::to_string(TEST.size()), __LINE__);
 		}else{ std::cout << text << std::endl; exit(0); //pre(tab_itr.second, __LINE__);
-		} return (1 == argc); }()){ //mpre("ОШИБКА формирования CSV", __LINE__);
-	/*}else if(std::map<std::string, std::string> tabs = [&](std::map<std::string, std::string> tabs = {}){ // Массив значений в полях итогов
-		if(std::map<int, std::map<std::string, std::string>> TAB; false){ mpre("ОШИБКА создания вложенного массива значений из БД", __LINE__);
-		}else if(sqlite3* db = 0; false){ mpre("ОШИБКА установки идентификатора ошибки базы данных", __LINE__);
-		}else if(SQLITE_OK != sqlite3_open(dbname.c_str(), &db)){ mpre(" ОШИБКА открытия базы данных "+ dbname, __LINE__);
-		}else if(sqlite3_stmt* stmt; false){ mpre("Ссылка на ошибки", __LINE__);
-		}else if(std::string sql = "SELECT * FROM mp_bmf_dano_values"; sql.empty()){ mpre("ОШИБКА составления запроса", __LINE__);
-		}else if(int result = sqlite3_prepare(db, sql.c_str(), -1, &stmt, 0); (SQLITE_OK != result)){ mpre("Запрос выполнен без ошибок", __LINE__);
-		}else if(int count = sqlite3_column_count(stmt); (0 >= count)){ mpre("ОШИБКА расчета количества записей в таблице", __LINE__);
-		}else if([&](){ //sqlite3_step(stmt);
-			while(SQLITE_ROW == sqlite3_step(stmt)){
-				std::map<std::string, std::string> row;
-				for(int num = 0; num < count; num++){
-					std::string field = std::string((const char*)sqlite3_column_name(stmt, num));
-					std::string val = std::string((const char*)sqlite3_column_text(stmt, num));
-					std::string value = (val == "NULL" ? "" : val);
-					row.insert(std::make_pair(field, value));
-				} TAB.insert(std::make_pair(TAB.size(), row));
-			}; return false; }()){ mpre("ОШИБКА получения данных запроса", __LINE__);
-		}else if([&](){ for(auto tab_itr:TAB){
-				if(tab_itr.second.end() == tab_itr.second.find("name")){ mpre("ОШИБКА поле name в списке полей не найдено", __LINE__);
-				}else if(std::string name = tab_itr.second.at("name"); name.empty()){ mpre("ОШИБКА выборки имени", __LINE__);
-				}else if(std::size_t pos = name.find("("); std::string::npos == pos){ mpre("ОШИБКА скобка в имени не найдена", __LINE__);
-				}else if(std::string title = name.substr(0, pos); title.empty()){ mpre("ОШИБКА получения заголовка", __LINE__);
-				}else if(tabs.insert(make_pair(title, name)); tabs.empty()){ mpre("ОШИБКА добавления заголовка в массив", __LINE__);
-				}else{ //pre(tab_itr.second, __LINE__); mpre("Имя " +name, __LINE__);
-				}
-			} return false; }()){ mpre("ОШИБКА сокращения имен из таблицы", __LINE__);
-		}else{ //for(auto tab_itr:TAB){ dump(tab_itr.second, __LINE__); }; mpre("Количество строк "+ std::to_string(TAB.size()) ,__LINE__);
-		} return tabs; }(); tabs.empty()){ mpre("ОШИБКА получения списка заголовков БД", __LINE__);*/
+		} return TAB; }(); false){ //mpre("ОШИБКА формирования CSV", __LINE__);
 	}else if(std::string file_name = [&](std::string file_name = ""){ // Получение имени файла csv из консоли
 		if(1 >= argc){ mpre("Не указан файл test.csv", __LINE__);
 		}else if(file_name = argv[1]; file_name.empty()){ mpre("ОШИБКА получения имени csv файла", __LINE__);
@@ -131,23 +95,16 @@ int main(int argc, char **argv){
 				if(std::string key = titles_itr.second; key.empty()){ mpre("Ключ пары не найден", __LINE__);
 				}else if(data.end() == data.find(titles_itr.first)){ mpre("ОШИБКА в csv массиве не найдено поле заголовока", __LINE__);
 				}else if(std::string val = data.at(titles_itr.first); key.empty()){ mpre("Ключ пары не найден", __LINE__);
-				//}else if(tabs.end() == tabs.find(key)){ dump(tabs, __LINE__); mpre("ОШИБКА заголовок не найден в таблицу "+ key, __LINE__);
-				//}else if(std::string tab = tabs.at(key); tab.empty()){ mpre("ОШИБКА пустой заголовок", __LINE__);
 				}else if(csv.insert(make_pair(key, val)); csv.empty()){ mpre("ОШИБКА добавления пары в массив", __LINE__);
 				}else{ //pre(csv, __LINE__); //mpre("Пара key=" + key+" val=" +val, __LINE__);
 				}} return false; }()){ mpre("ОШИБКА устанвоки пар заголовок:значение", __LINE__);
-			//}else if(CSV.insert(make_pair(CSV.size(), csv)); CSV.empty()){ mpre("ОШИБКА добавления новой строки", __LINE__);
 			}else if(int id = [&](int id = 0){ // Выборка идентификатора
 				if(csv.end() == csv.find("id")){ dump(csv, __LINE__); mpre("ОШИБКА поле id не найдено в свойствах примера", __LINE__);
 				}else if(id = atoi(csv.at("id").c_str()); !id){ dump(csv, __LINE__); mpre("ОШИБКА выборки идентификатора примера", __LINE__);
 				}else if(csv.erase("id"); false){ mpre("ОШИБКА удаления идентификатора из данных", __LINE__);
 				}else{ //mpre("Идентификатор примера " +id, __LINE__);
 				} return id; }(); !id){ mpre("ОШИБКА выборки идентификатора", __LINE__);
-			/*}else if([&](std::string list = ""){ // Принудительный пропуск
-				if(4 >= argc){ //mpre("Нет условий на принудительный пропуск", __LINE__);
-				}else if(list = argv[4]; list.empty()){ mpre("ОШИБКА выборки списка принудительного пропуска", __LINE__);
-				}else{ return (std::string::npos == list.find(id)); mpre("Принудительный пропуск " +list, __LINE__);
-				} return false; }()){ mpre("Принудительный пропуск " +id + " (" +argv[4] +")", __LINE__);*/
+			}else if(TAB.end() != TAB.find(id)){ mpre("Тест уже расчитан " +std::to_string(id), __LINE__);
 			}else if(int skip = [&](int split =1, int inc = 0){ // Пропуск по split
 				if(4 >= argc){ //mpre("Остаток не указан ", __LINE__); // assert(!("Длина не может быть отрицательна", 1011)); //assert(!"Проверяем на ноль"); //assert(!"ОШИБКА какая то"); //throw __LINE__;// ohrow "Ошибка - на 0 делить нельзя!!!!"; //std::perror("ОШИБКА"); //mpre("Делитель не задан", __LINE__);
 				}else if(split = atoi(argv[4]); (0 > split)){ mpre("ОШИБКА выборки делителя", __LINE__);
@@ -156,7 +113,6 @@ int main(int argc, char **argv){
 				}else{ //mpre("Проверка пропуска id=" + std::to_string(id)+ " split=" +std::to_string(split) +" " +std::to_string(inc) +" " +std::to_string(id%split), __LINE__);
 				}return (id%split != inc); }()){ mpre("Пропуск выполнения "+ std::to_string(id)+ "%" +(4 < argc ? argv[4] : "0") +" != " +(5 < argc ? argv[5] : "0"), __LINE__);
 			}else if(mpre("Расчет "+ std::to_string(id), __LINE__); false){ mpre("ОШИБКА уведомления", __LINE__);
-			//}else if(csv.end() == csv.find("Id")){ mpre("ОШИБКА идентификатор в списке не найден", __LINE__);
 			}else if(nlohmann::json json = csv; json.empty()){ mpre("ОШИБКА загрузки данных в json", __LINE__);
 			}else if(std::string data = "{\""+ std::to_string(id)+ "\":{\"dano\":" +json.dump() +"}}"; data.empty()){ mpre("ОШИБКА получения строки данных", __LINE__);
 			}else if(std::string cmd = "./bimorph "+ dbname+ " -dano '" +data +"' 2>/dev/null"; cmd.empty()){ mpre("ОШИБКА формирования строки комманды расчета", __LINE__);
@@ -188,13 +144,6 @@ int main(int argc, char **argv){
 			}else{ //pre(csv, __LINE__); //mpre("Загружаемая строка "+ line, __LINE__);
 			}
 		} return CSV; }(); CSV.empty()){ mpre("ОШИБКА обработки файла", __LINE__);
-	//}else if(nlohmann::json JSON = CSV; false){ mpre("ОШИБКА обьявления данных json", __LINE__);
-	/*}else if([&](int loop = 3){ for(auto csv_itr:CSV){ // Поэлементная обработка csv
-			if(false){ mpre("Пропуск расчетов", __LINE__);
-			}else if(std::map<std::string, std::string> csv = csv_itr.second; csv.empty()){ mpre("ОШИБКА получения массива данных", __LINE__);
-			}else{ mpre("Итоговая цена id=" +id +" " +price, __LINE__);
-			}
-		} return false; }()){ mpre("ОШИБКА обработки csv построчно", __LINE__);*/
 	}else{ mpre("Количество CSV строк в файле "+ std::to_string(CSV.size()), __LINE__);
 	}
 }
